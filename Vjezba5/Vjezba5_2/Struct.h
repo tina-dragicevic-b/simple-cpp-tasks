b44a8c3d6263fe0e32809ff8756050f64aca0eff
tina-dragicevic <61937040+tina-dragicevic@users.noreply.github.com>
2020-05-24 15:15:36 +0200

    Add files via upload

---
+++ Vjezba5/Vjezba5_2/Struct.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <string.h>
+#include <vector>
+//#include <algorithm>
+//#include <random>
+//#include <bits/stdc++.h>
+
+
+struct Point{
+    double x, y;
+    char ch = 'o';
+    //Point(double x, double y) : x(x), y(y) {};
+    void drawChar(double x, double y, char ch);/*{
+         std::cout << "draw char" << std::endl;
+        x = int(x + 0.5); y = int(y + 0.5);
+        for(int i = 0; i <= x; i++){
+            for(int j = 0; j <= y; j++){
+                if(i == (int)x && (int)j == y){
+                    std::cout << ch;
+                }
+                else{std::cout << '-';}
+            }
+            std::cout << "\n";
+        }
+    } */
+}Point; 
